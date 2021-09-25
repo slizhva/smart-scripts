6617f620b0e155b44e70d51e3d37e9a50852468c
slizhva <slizhva@gmail.com>
2021-09-25 23:30:24 +0300

    Remove dev comments.

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-//ini_set('display_errors', true);
-//error_reporting(E_ALL);
-
 // Include router class
 require_once ('Route.php');
 
 // Example of usage https://steampixel.de/simple-and-elegant-url-routing-with-php/
-// Accept only numbers as parameter. Other characters will result in a 404 error
 Route::add('/([a-z]*)/([a-z]*)', function($param1, $param2) {
     $class = ucfirst(strtolower($param1));
 
