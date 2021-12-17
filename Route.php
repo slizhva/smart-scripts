03565ff2f441cf9abee609d5987a47b699935c23
slizhva <slizhva@gmail.com>
2021-12-18 01:42:45 +0300

    Update route class.

--- Route.php
+++ Route.php
@@ -2,42 +2,41 @@
 
 class Route {
 
-    private static $routes = Array();
+    private static array $routes = [];
     private static $pathNotFound = null;
     private static $methodNotAllowed = null;
 
-    public static function add($expression, $function, $method = 'get'){
-        array_push(self::$routes,Array(
+    public static function add(string $expression, callable $function, string $method = 'get'): void
+    {
+        self::$routes[] = array(
             'expression' => $expression,
             'function' => $function,
             'method' => $method
-        ));
+        );
     }
 
-    public static function pathNotFound($function){
+    public static function pathNotFound(callable $function): void
+    {
         self::$pathNotFound = $function;
     }
 
-    public static function methodNotAllowed($function){
+    public static function methodNotAllowed(callable $function): void
+    {
         self::$methodNotAllowed = $function;
     }
 
-    public static function run($basepath = '/'){
+    public static function run(string $basepath = '/'): void
+    {
 
         // Parse current url
         $parsed_url = parse_url($_SERVER['REQUEST_URI']);//Parse Uri
 
-        if(isset($parsed_url['path'])){
-            $path = $parsed_url['path'];
-        }else{
-            $path = '/';
-        }
+        $path = $parsed_url['path'] ?? '/';
 
         // Get current request method
         $method = $_SERVER['REQUEST_METHOD'];
 
         $path_match_found = false;
-
         $route_match_found = false;
 
         foreach(self::$routes as $route){
@@ -45,7 +44,7 @@ class Route {
             // If the method matches check the path
 
             // Add basepath to matching string
-            if($basepath!=''&&$basepath!='/'){
+            if ($basepath!=='' && $basepath!=='/'){
                 $route['expression'] = '('.$basepath.')'.$route['expression'];
             }
 
@@ -53,21 +52,19 @@ class Route {
             $route['expression'] = '^'.$route['expression'];
 
             // Add 'find string end' automatically
-            $route['expression'] = $route['expression'].'$';
-
-            // echo $route['expression'].'<br/>';
+            $route['expression'] .= '$';
 
             // Check path match
-            if(preg_match('#'.$route['expression'].'#',$path,$matches)){
+            if (preg_match('#'.$route['expression'].'#',$path,$matches)) {
 
                 $path_match_found = true;
 
                 // Check method match
-                if(strtolower($method) == strtolower($route['method'])){
+                if (strtolower($method) === strtolower($route['method'])) {
 
                     array_shift($matches);// Always remove first element. This contains the whole string
 
-                    if($basepath!=''&&$basepath!='/'){
+                    if ($basepath!=='' && $basepath!=='/') {
                         array_shift($matches);// Remove basepath
                     }
 
@@ -82,18 +79,18 @@ class Route {
         }
 
         // No matching route was found
-        if(!$route_match_found){
+        if (!$route_match_found) {
 
             // But a matching path exists
-            if($path_match_found){
+            if ($path_match_found) {
                 header("HTTP/1.0 405 Method Not Allowed");
-                if(self::$methodNotAllowed){
-                    call_user_func_array(self::$methodNotAllowed, Array($path,$method));
+                if (self::$methodNotAllowed) {
+                    call_user_func(self::$methodNotAllowed, $path, $method);
                 }
-            }else{
+            } else {
                 header("HTTP/1.0 404 Not Found");
-                if(self::$pathNotFound){
-                    call_user_func_array(self::$pathNotFound, Array($path));
+                if (self::$pathNotFound) {
+                    call_user_func(self::$pathNotFound, $path);
                 }
             }
 
